1db28f58962432dc0e7548bb6ab7a7f3794162ac
mr1c10 <mauriciorodriguesoficial@gmail.com>
2025-04-04 15:09:37 -0300

    fix: update blog entry to reflect new title and description for improved clarity

--- config.blog.ts
+++ config.blog.ts
@@ -8,10 +8,15 @@ const blog: Blog = {
             description: "I'm a Brazilian backend developer focused on building efficient and scalable solutions.💻",
             footer: "I primarily work with C#, Java, PHP, and database management.",
         },
+        // {
+        //     title: "Sou um desenvolvedor backend",
+        //     description: "Desenvolvedor backend brasileiro, apaixonado por tecnologia e especializado em criar sistemas robustos, eficientes e escaláveis.💻",
+        //     footer: "Trabalho principalmente com C#, Java, PHP e gerenciamento de banco de dados."
+        // },
         {
-            title: "Sou um desenvolvedor backend",
-            description: "Desenvolvedor backend brasileiro, apaixonado por tecnologia e especializado em criar sistemas robustos, eficientes e escaláveis.💻",
-            footer: "Trabalho principalmente com C#, Java, PHP e gerenciamento de banco de dados."
+            title: "Garoto de Programa 😏",
+            description: "Faço suas ideias e pensamentos se tornarem realidade... Trabalho principalmente com C#, Java, PHP e gerenciamento de banco de dados.",
+            footer: "Desenvolvedor backend"
         },
     ],
 }
