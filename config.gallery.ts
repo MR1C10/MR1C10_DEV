1e5053d6000bba46f7ed87668fecc7b816a5de2b
mr1c10 <mauriciorodriguesoficial@gmail.com>
2025-04-04 13:31:09 -0300

    feat: add new gallery entries and multiple GIF assets for enhanced visual content

--- config.gallery.ts
+++ config.gallery.ts
@@ -9,6 +9,10 @@ const gallery: { albums: Album[] } = {
         "https://i.pinimg.com/236x/02/63/7a/02637a8d4b36894dca7dd8b965daec4a.jpg",
       posts: [
         {
+          title: "",
+          src: "/assets/img/",
+        },
+        {
           title: "teste 1",
           src: "/assets/img/toduro.png",
         }
@@ -23,6 +27,22 @@ const gallery: { albums: Album[] } = {
         {
           title: "teste 2",
           src: "/assets/img/poatan.png",
+        }
+      ]
+    },
+    {
+      title: "Teste3",
+      route: "n",
+      coverImageSrc:
+        "https://preview.redd.it/queria-saber-o-contexto-do-meme-desse-cara-dando-contexto-v0-hy5rf7z3fiud1.jpeg?auto=webp&s=cb7366dc8eefd46ac2f627e64282d5ed55e1777a",
+      posts: [
+        {
+          title: "FDS?!",
+          src: "/assets/img/eu.gif",
+        },
+        {
+          title: "Kit sobrevivência",
+          src: "/assets/img/kitsobrev.jpg",
         }
       ]
     }
